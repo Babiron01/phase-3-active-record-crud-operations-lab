83881045de7de44659050add2aa02a37373a6803
Babikip01 <126768788+Babikip01@users.noreply.github.com>
2023-06-04 21:42:20 +0300

    Completed assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_01_121827) do
+ActiveRecord::Schema.define(version: 2023_06_04_174923) do
 
-   
-  
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
   end
+
+end
